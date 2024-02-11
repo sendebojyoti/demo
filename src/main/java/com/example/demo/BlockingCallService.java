627f1b9efd7ed5f3f51d065a87d0d62bbf8fefd2
sendebojyoti <deb.m.sen@gmail.com>
2024-02-11 15:59:28 +0530

    Initial Version

---
+++ src/main/java/com/example/demo/BlockingCallService.java
@@ -0,0 +1,48 @@
+package com.example.demo;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Service
+@Slf4j
+public class BlockingCallService {
+
+    private static final org.slf4j.Logger log = org.slf4j.LoggerFactory.getLogger(BlockingCallService.class);
+
+    private final WebClient webClient;
+    private final RestTemplate restTemplate;
+
+    public BlockingCallService(WebClient webClient, RestTemplate restTemplate) {
+        this.webClient = webClient;
+        this.restTemplate = restTemplate;
+    }
+
+    // Using Web
+    // Client for making a blocking call
+    public String webClientBlockingCall() {
+        long startTime = System.currentTimeMillis();
+        String response = webClient.get()
+                .uri("/posts/1")
+                .retrieve()
+                .bodyToMono(String.class)
+                .block(); // Blocking call
+        long endTime = System.currentTimeMillis();
+        log.info("Web Template Time: " + Long.toString(endTime - startTime));
+
+        return "Response from WebClient: " + response;
+    }
+
+    // Using RestTemplate for making a blocking call
+
+    public String restTemplateBlockingCall() {
+        RestTemplate restTemplate = new RestTemplate();
+        long startTime = System.currentTimeMillis();
+        String response = restTemplate.getForObject("https://jsonplaceholder.typicode.com/posts/1", String.class);
+        long endTime = System.currentTimeMillis();
+        log.info("Rest Template Time: " + Long.toString(endTime - startTime));
+
+        return "Response from RestTemplate: " + response;
+    }
+}
---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,43 @@
+package com.example.demo;
+
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@Slf4j
+@RestController
+public class DemoApplication {
+    private static final Logger log = LoggerFactory.getLogger(DemoApplication.class);
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+}
+
+@Component
+class CommandLineAppStartupRunner implements CommandLineRunner {
+    @Autowired
+    private BlockingCallService blockingCallController;
+
+    @Override
+    public void run(String... args) throws Exception {
+        int i = 0,j = 0;
+        do {
+            blockingCallController.webClientBlockingCall();
+            i++;
+        } while (i <= 100);
+
+        do {
+            blockingCallController.restTemplateBlockingCall();
+            j++;
+        } while (j <= 100);
+    }
+}
